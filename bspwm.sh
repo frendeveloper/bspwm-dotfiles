32ae024111516c60154e848c1bad023279a05b17
frendeveloper <pedja.spasovic@gmail.com>
2023-02-11 18:22:18 +0000

    Update

--- bspwm.sh
+++ bspwm.sh
@@ -27,6 +27,8 @@ cp -R dotconfig/* "/home/$username/.config/"
 cp bg.jpg "/home/$username/Pictures/"
 chown -R "$username:$username" "/home/$username"
 
+sudo apt install git build-essential manpages-dev
+
 # Installing sugar-candy dependencies
 nala install libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
 # Installing Essential Programs 
@@ -34,11 +36,10 @@ nala install feh bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x
 # Installing Other less important Programs
 nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme fonts-noto-color-emoji sddm variety jq dunst -y
 
-
-sudo apt install git build-essential manpages-dev
 sudo apt install curl git kitty telegram-desktop bspwm sxhkd feh polybar xbacklight light pamix picom dunst rofi flameshot network-manager network-manager-gnome xfce4-power-manager xfce4-settings zsh zsh-autosuggestions zsh-syntax-highlighting -y
 sudo systemctl enable NetworkManager.service
 sudo snap install ksuperkey
+
 # Installing fonts
 cd "$builddir" || exit
 nala install fonts-font-awesome -y
@@ -48,11 +49,21 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 unzip Meslo.zip -d "/home/$username/.fonts"
 mv dotfonts/fontawesome/otfs/*.otf "/home/$username/.fonts/"
 chown "$username:$username" "/home/$username/.fonts/*"
-
 # Reloading Font
 fc-cache -vf
 # Removing zip Files
 rm ./FiraCode.zip ./Meslo.zip
+
+
+#Betterlockscreen
+wget https://github.com/betterlockscreen/betterlockscreen/archive/refs/heads/main.zip
+unzip main.zip
+cd betterlockscreen-main/
+chmod u+x betterlockscreen
+cp betterlockscreen /usr/local/bin/
+cp system/betterlockscreen@.service /usr/lib/systemd/system/
+betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
+sudo systemctl enable betterlockscreen@$USER.service
 
 # Install Nordzy cursor
 git clone https://github.com/alvatip/Nordzy-cursors
@@ -84,5 +95,3 @@ cp .zshrc-personal ~
 mkdir ~/.local/bin
 cp -R .local/bin/* ~/.local/bin
 chmod -R +x ~/.local/bin
-betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
-sudo systemctl enable betterlockscreen@$USER.service
